055d9b09ac8c53b4e3a5e24cd359792847d4a099
Brixter Porras <brixterporras@gmail.com>
2023-03-23 23:27:54 +0800

    feat: added category test

--- tests/categories_service.rs
+++ tests/categories_service.rs
@@ -11,7 +11,7 @@ use budgetto_api::{
 use mockall::predicate::*;
 
 #[tokio::test]
-async fn return_success_when_downstream_services_succeedand_user_exists() {
+async fn return_success_when_downstream_services_succeedand_category_exists() {
     // arrange
     let mut fixture = CategoriesServiceTestFixture::default();
 
@@ -21,6 +21,34 @@ async fn return_success_when_downstream_services_succeedand_user_exists() {
         .with(eq(1), eq(String::from("stub category")))
         .times(1)
         .return_once(move |_, _| Ok(Category::default()));
+
+    fixture
+        .mock_repository
+        .expect_get_category_by_id()
+        .with(eq(1))
+        .times(1)
+        .return_once(move |_| Ok(Some(Category::default())));
+
+    fixture
+        .mock_repository
+        .expect_get_categories()
+        .with(eq(1))
+        .times(1)
+        .return_once(move |_| Ok(vec![Category::default()]));
+
+    fixture
+        .mock_repository
+        .expect_update_category()
+        .with(eq(1), eq(String::from("stub category")))
+        .times(1)
+        .return_once(move |_, _| Ok(Category::default()));
+
+    fixture
+        .mock_repository
+        .expect_delete_category()
+        .with(eq(1))
+        .times(1)
+        .return_once(move |_| Ok(()));
 
     let categories_service =
         CategoriesService::new(Arc::new(fixture.mock_repository) as DynCategoriesRepository);
