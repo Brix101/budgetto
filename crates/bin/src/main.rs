550a2657ff5a46affa01ca1d779154bb2a38f8d4
Brixter Porras <brixterporras@gmail.com>
2023-04-19 00:04:14 +0800

    feat: initialize api lib

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -5,8 +5,9 @@ use clap::Parser;
 use dotenvy::dotenv;
 use tracing::info;
 
+use api::router::ApplicationController;
 use core::{config::AppConfig, logger};
-use infrastructure::connection_pool::ConnectionManager;
+use infrastructure::{connection_pool::ConnectionManager, service_register::ServiceRegister};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -17,10 +18,15 @@ async fn main() -> anyhow::Result<()> {
     let _guard = logger::init(config.cargo_env);
 
     info!("environment loaded and configuration parsed, initializing Postgres connection and running migrations...");
-    let _pg_pool = ConnectionManager::new_pool(&config.database_url, config.run_migrations)
+    let pg_pool = ConnectionManager::new_pool(&config.database_url, config.run_migrations)
         .await
         .expect("could not initialize the database connection pool");
 
-    println!("{:#?}", config);
+    let port = config.port;
+    let service_register = ServiceRegister::new(pg_pool, config.clone());
+
+    info!("migrations successfully ran, initializing axum server...");
+    ApplicationController::serve(port, &config.cors_origin, service_register).await?;
+
     Ok(())
 }
--- crates/domain/src/lib.rs
+++ crates/domain/src/lib.rs
@@ -21,3 +21,20 @@ impl ApiError {
         Self { errors: error_map }
     }
 }
+
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+pub struct PingResponse {
+    pub message: String,
+}
+
+impl PingResponse {
+    pub fn new(message: String) -> Self {
+        Self { message }
+    }
+}
+
+impl Default for PingResponse {
+    fn default() -> Self {
+        Self::new(String::from("API is responsive"))
+    }
+}
