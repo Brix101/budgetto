8e2f7cdbf14999bc5f064166a79d5dc7442da13a
Brixter Porras <brixterporras@gmail.com>
2023-04-18 14:08:13 +0800

    initialize app config

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+// use anyhow::Context;
+use clap::Parser;
+use dotenvy::dotenv;
+
+use core::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    let config = Arc::new(AppConfig::parse());
+
+    println!("{:#?}", &config.database_url);
+    Ok(())
 }
