5e7dcae8edaffeae149734eb512d923227766502
Brixter Porras <brixterporras@gmail.com>
2024-08-28 15:50:04 +0800

    feat: added api web connection

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import { trpc } from "./utils/trpc";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const greetQuery = trpc.greeting.useQuery();
+
+  console.log(greetQuery.data);
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
