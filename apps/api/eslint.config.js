384f485a123f412622250f5275b173af9c3f3870
Brixter Porras <brixterporras@gmail.com>
2024-12-20 10:53:01 +0800

    feat: added initial api tsconfig

---
+++ apps/api/eslint.config.js
@@ -0,0 +1,17 @@
+import baseConfig from "@acme/eslint-config/base";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ["dist/**"],
+  },
+  ...baseConfig,
+  {
+    rules: {
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
